ca191ae2ff6459aaff7a5efb5b021fd6ade39430
DESKTOP-S8SHC68\wolfm <wolfmar.ww@gmail.com>
2018-11-10 22:51:24 +0100

    Start screen

---
+++ WFSGJ_2018/Assets/ButtonUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ButtonUI : MonoBehaviour
+{
+    static ButtonUI _active;
+
+    public Image button;
+    public Color[] colors;
+
+    public UnityEvent onClickEvent;
+
+    public void Select()
+    {
+        if (_active) _active.Diselect();
+        _active = this;
+        button.color = colors[1];
+    }
+
+    public void Diselect()
+    {
+        button.color = colors[0];
+    }
+
+    public void OnClick()
+    {
+        if (_active == this && _active.gameObject.activeInHierarchy)
+            onClickEvent.Invoke();
+    }
+
+    public static void OnClickActive()
+    {
+        if (_active) _active.OnClick();
+    }
+}
---
+++ WFSGJ_2018/Assets/StartScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : GameScreen
+{
+    public ButtonUI[] buttons;
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Player1_Vertical") > 0.2) buttons[0].Select();
+        else if (Input.GetAxisRaw("Player1_Vertical") < -0.2f) buttons[1].Select();
+
+        if (Input.GetButtonDown("Player1_Confirm")) ButtonUI.OnClickActive();
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        buttons[0].Select();
+    }
+
+    public void OnPlayButton()
+    {
+        GameManager.Instance.ShowScreen(ScreenType.characterSelect, false);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
