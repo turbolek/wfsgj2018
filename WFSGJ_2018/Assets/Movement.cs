44495a8698c0afb44872d72afc5e0be7c3bc8b22
Daniel Kunicki <dankunicki@gmail.com>
2018-11-10 01:45:17 +0100

    Add basic player movement

---
+++ WFSGJ_2018/Assets/Movement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float maxVelocity;
+    public float acceleration;
+    public float velocityDrag;
+    
+    float velocityX;
+    float velocityY;
+
+    string verticalAxisName = "";
+    string horizontalAxisName = "";
+
+    // Use this for initialization
+    void Start () {
+        Player player = GetComponent<Player>();
+        if (player == null)
+            return;
+
+        switch (player.playerID)
+        {
+            case Player.PlayerID.Player1:
+                {
+                    verticalAxisName = "Player1_Vertical";
+                    horizontalAxisName = "Player1_Horizontal";
+                    break;
+                }
+            case Player.PlayerID.Player2:
+                {
+                    verticalAxisName = "Player2_Vertical";
+                    horizontalAxisName = "Player2_Horizontal";
+                    break;
+                }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageMovement();
+        ApplyDrag();
+	}
+
+    void ManageMovement()
+    {
+        float horizontalAxis = Input.GetAxis(horizontalAxisName);
+        float verticalAxis = Input.GetAxis(verticalAxisName);
+
+        velocityX += Mathf.Clamp(horizontalAxis * acceleration, -maxVelocity, maxVelocity);
+        velocityY += Mathf.Clamp(verticalAxis * acceleration, -maxVelocity, maxVelocity);
+        transform.Translate(new Vector2(velocityX, velocityY) * Time.deltaTime);
+    }
+
+    void ApplyDrag()
+    {
+        if (velocityX > velocityDrag)
+            velocityX -= velocityDrag;
+        else if (velocityX < -velocityDrag)
+            velocityX += velocityDrag;
+        else
+            velocityX = 0;
+
+        if (velocityY > velocityDrag)
+            velocityY -= velocityDrag;
+        else if (velocityY < -velocityDrag)
+            velocityY += velocityDrag;
+        else
+            velocityY = 0;
+    }
+}
---
+++ WFSGJ_2018/Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerID playerID;
+    public enum PlayerID
+    {
+        Player1,
+        Player2
+    }
+}
