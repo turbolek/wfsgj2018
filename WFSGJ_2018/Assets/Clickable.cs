3b45c3649313c2d32b88a1031bc9b79b94710e11
rilane <collab.cat@gmail.com>
2018-11-10 13:01:49 +0100

    rebind click on A

---
+++ WFSGJ_2018/Assets/Clickable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour {
+    public int playerDistance = 100;
+
+    public GameObject bound;
+    
+    void Start () {
+        Transform e = Instantiate(bound, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation).transform;
+        e.localScale = new Vector3(1, 1, playerDistance);
+        e.SetParent(this.transform);
+        e.name = "collider";
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+}
