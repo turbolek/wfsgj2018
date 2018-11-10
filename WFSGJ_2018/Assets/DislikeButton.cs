7e26d13062f91c7b93cacd0079e29d30719b0de9
Daniel Kunicki <dankunicki@gmail.com>
2018-11-10 14:42:11 +0100

    * Add dislike button and clicking mechanics

---
+++ WFSGJ_2018/Assets/DislikeButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DislikeButton : MonoBehaviour
+{
+
+    public void Click()
+    {
+        Debug.Log("Dislike button clicked");
+    }
+}
--- WFSGJ_2018/Assets/Player.cs
+++ WFSGJ_2018/Assets/Player.cs
@@ -5,9 +5,77 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerID playerID;
+
+    #region Controls
+    string verticalAxisName = "";
+    public string VerticalAxisName {  get { return verticalAxisName; } }
+
+    string horizontalAxisName = "";
+    public string HorizontalAxisName { get { return horizontalAxisName; } }
+    
+    string clickName = "";
+    public string ClickName { get { return clickName; } }
+    #endregion
+
+    DislikeButton dislikeButton = null;
+
     public enum PlayerID
     {
         Player1,
         Player2
+    }
+
+    void CheckClick()
+    {
+        if (Input.GetButtonDown(clickName))
+        {
+            Debug.Log("Player: " + name + " clicked");
+            if (dislikeButton != null)
+                dislikeButton.Click();
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(name + " entered trigger");
+        dislikeButton = collision.GetComponent<DislikeButton>();
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        Debug.Log(name + " exit trigger");
+        if (collision.GetComponent<DislikeButton>() == dislikeButton)
+            dislikeButton = null;
+    }
+
+    private void Update()
+    {
+        CheckClick();
+    }
+
+    private void Start()
+    {
+        InitControls();
+    }
+
+    void InitControls()
+    {
+        switch (playerID)
+        {
+            case PlayerID.Player1:
+                {
+                    verticalAxisName = "Player1_Vertical";
+                    horizontalAxisName = "Player1_Horizontal";
+                    clickName = "Player1_Click";
+                    break;
+                }
+            case Player.PlayerID.Player2:
+                {
+                    verticalAxisName = "Player2_Vertical";
+                    horizontalAxisName = "Player2_Horizontal";
+                    clickName = "Player2_Click";
+                    break;
+                }
+        }
     }
 }
